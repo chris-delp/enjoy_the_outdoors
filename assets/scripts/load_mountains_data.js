0e84aef05b53a2625b16ffc3f48168a0530e5a74
Delp <Christopher.Delp@LibertyMutual.com>
2022-09-20 11:37:59 -0400

    fixed dropdown issue on mountains and added parks table

--- assets/scripts/load_mountains_data.js
+++ assets/scripts/load_mountains_data.js
@@ -1,7 +1,7 @@
 "use strict"
 
 let mountainsArray = []
-
+let clickCounter = 0;
 window.onload = function(){
 
     loadJsonData("./assets/data/mountains.json").then((mountains) => {
@@ -11,11 +11,15 @@ window.onload = function(){
 
 }
 function populateSelect() {
-          
+         
     let mountainDropdownList = document.getElementById('sel');
+    if(clickCounter === 0) {
         for(let i=0; i<mountainsArray.length; i++){
             mountainDropdownList.innerHTML = mountainDropdownList.innerHTML + '<option>' + mountainsArray[i]['name'] + '</option>';
+        }
     }
+    clickCounter++;
+    console.log(clickCounter);
 } 
 
 //function that can "fetch" the sunset/sunrise times
